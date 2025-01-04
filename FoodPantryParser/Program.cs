c402c3796d0142b6f3a12abe6e82495ac3b24e0a
diyradiolab <diyradiolab@gmail.com>
2025-01-04 14:57:59 -0500

    Updated consecutive skips rows to 10. 1 didn't allow any room for error.

--- FoodPantryParser/Program.cs
+++ FoodPantryParser/Program.cs
@@ -12,7 +12,7 @@ namespace FoodPantryParser
     {
         public static string DataFolder => @"C:\Users\Alan Hess\Desktop\FoodPantryData";
         public static string OutputFolder => $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\FPReport";
-        internal const int InvalidRowsBetweenOrders = 2; // After two rows are encountered with no data, we know there are no more entries. 
+        internal const int InvalidRowsBetweenOrders = 10; // After two rows are encountered with no data, we know there are no more entries. 1 OR 2 is common. 10 is generous.
 
         static void Main(string[] args)
         {
